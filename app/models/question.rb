ef36c03bdd882b662bc6fca83887b6c3dc47d045
Harry Brundage <harry.brundage@gmail.com>
2011-11-28 21:16:37 -0500

    Made dependent relationships a little more formal

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
-  has_many :answers
+  has_many :answers, :dependent => :destroy
   has_many :responders, :through => :answers
   validates_presence_of :text, :order
 
--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,4 +1,5 @@
 class Responder < ActiveRecord::Base
   has_many :answers
+  has_many :responses, :dependent => :destroy
   validate :phone_number, :presence => true, :unique => true
 end
