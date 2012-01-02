ed67f740063c9a95f520bcee877f3de6170e4403
Harry Brundage <harry.brundage@gmail.com>
2012-01-02 17:32:40 -0500

    Whoops

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,3 @@
-if Rails.env.production?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-else
-  uri = URI.parse("localhost:6789")
-end
+uri = URI.parse(ENV["REDISTOGO_URL"] || "localhost:6789")
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Resque.redis = $redis
